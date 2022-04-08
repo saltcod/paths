22ba5ada1c259abc6e4608fdf6c8252fe707af9b
Terry Sutton <saltcod@gmail.com>
2022-04-08 16:00:36 -0230

    Fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,3 @@
-// If you want to use other PostCSS plugins, see the following:
-// https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {},
-		"postcss-import": {},
-		"postcss-mixins": {},
-		"postcss-nesting": {},
-		"postcss-preset-env": {},
-		"postcss-object-fit-images": {},
-	},
+	plugins: ["tailwindcss", "autoprefixer"],
 };
